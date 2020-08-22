7dcbc4ee715bb233076dd2aa684a87646cd6f6e1
Marco A. Braghim <marco.a.braghim@gmail.com>
2020-08-22 19:58:02 -0300

    Reorganizado lista de itens de lista

--- lib/widgets/ItemsList.dart
+++ lib/widgets/ItemsList.dart
@@ -27,9 +27,7 @@ class _ItemsListState extends State<ItemsList> {
   Widget build(BuildContext context) {
     // Item default
     if (widget.items.isEmpty) {
-      return ListView(shrinkWrap: true, children: <Widget>[
-        ListTile(title: Text('Nenhum item para exibir ainda'))
-      ]);
+      return ListView(shrinkWrap: true, children: <Widget>[ListTile(title: Text('Nenhum item para exibir ainda'))]);
     }
 
     // The list after filter apply
@@ -50,9 +48,7 @@ class _ItemsListState extends State<ItemsList> {
 
     // Empty after filters
     if (filteredList.isEmpty) {
-      return ListView(shrinkWrap: true, children: <Widget>[
-        ListTile(title: Text('Nenhum item encontrado...'))
-      ]);
+      return ListView(shrinkWrap: true, children: <Widget>[ListTile(title: Text('Nenhum item encontrado...'))]);
     }
 
     // Instancia model
@@ -74,82 +70,64 @@ class _ItemsListState extends State<ItemsList> {
           double realVal = currencyToDouble(item['valor']);
           String valTotal = doubleToCurrency(realVal * item['quantidade']);
 
-          return Slidable(
-            delegate: SlidableDrawerDelegate(),
-            actionExtentRatio: 0.2,
-            closeOnScroll: true,
-            child: ListTile(
-              leading: GestureDetector(
-                child: Icon(
-                    ((item['checked'] == 1)
-                        ? Icons.check_box
-                        : Icons.check_box_outline_blank),
-                    color: ((item['checked'] == 0)
-                        ? Layout.dark(0.5)
-                        : Layout.secondary()),
-                    size: 42),
-                onTap: () {
-                  itemBo.update({'checked': !(item['checked'] == 1)},
-                      item['pk_item']).then((bool updated) {
-                    if (updated) {
-                      widget.itemsListBloc.getList();
-                    }
-                  });
-                },
+          return ListTile(
+            leading: GestureDetector(
+              child: Icon(
+                ((item['checked'] == 1) ? Icons.check_box : Icons.check_box_outline_blank),
+                color: ((item['checked'] == 0) ? Layout.dark(0.5) : Layout.secondary()),
+                size: 42,
               ),
-              title: Text(item['name']),
-              subtitle: Text(
-                  '${item['quantidade']} $itemUnit X ${item['valor']} = $valTotal'),
-              trailing: Icon(Icons.arrow_forward_ios),
-              onTap: () {
-                itemBo.getItem(item['pk_item']).then((Map i) {
-                  // Adiciona dados do item a pagina
-                  ItemEditPage.item = i;
+              onTap: () async {
+                bool updated = await itemBo.update({'checked': !(item['checked'] == 1)}, item['pk_item']);
 
-                  // Abre a pagina
-                  Navigator.of(context).pushNamed(ItemEditPage.tag);
-                });
+                if (updated) {
+                  widget.itemsListBloc.getList();
+                }
               },
             ),
-            secondaryActions: <Widget>[
-              IconSlideAction(
-                caption: 'Deletar',
-                icon: Icons.delete,
-                color: Colors.red,
-                onTap: () {
-                  showDialog(
-                      context: context,
-                      barrierDismissible: true,
-                      builder: (BuildContext ctx) {
-                        return AlertDialog(
-                          title: Text('Tem certeza?'),
-                          content: Text(
-                              'Esta ação irá remover o item selecionado e não poderá ser desfeita'),
-                          actions: <Widget>[
-                            RaisedButton(
-                              color: Layout.secondary(),
-                              child: Text('Cancelar',
-                                  style: TextStyle(color: Layout.light())),
-                              onPressed: () {
-                                Navigator.of(ctx).pop();
-                              },
-                            ),
-                            RaisedButton(
-                                color: Layout.danger(),
-                                child: Text('Remover',
-                                    style: TextStyle(color: Layout.light())),
-                                onPressed: () {
-                                  itemBo.delete(item['pk_item']);
+            title: Text(item['name']),
+            subtitle: Text('${item['quantidade']} $itemUnit X ${item['valor']} = $valTotal'),
+            trailing: IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  barrierDismissible: true,
+                  builder: (BuildContext ctx) {
+                    return AlertDialog(
+                      title: Text('Tem certeza?'),
+                      content: Text('Esta ação irá remover o item selecionado e não poderá ser desfeita'),
+                      actions: <Widget>[
+                        RaisedButton(
+                          color: Layout.secondary(),
+                          child: Text('Cancelar', style: TextStyle(color: Layout.light())),
+                          onPressed: () => Navigator.of(ctx).pop(),
+                        ),
+                        RaisedButton(
+                          color: Layout.danger(),
+                          child: Text('Remover', style: TextStyle(color: Layout.light())),
+                          onPressed: () {
+                            itemBo.delete(item['pk_item']);
 
-                                  Navigator.of(ctx).pop();
-                                  widget.itemsListBloc.getList();
-                                })
-                          ],
-                        );
-                      });
-                },
-              )
-            ],
+                            Navigator.of(ctx).pop();
+                            widget.itemsListBloc.getList();
+                          },
+                        )
+                      ],
+                    );
+                  },
+                );
+              },
+            ),
+            onTap: () {
+              itemBo.getItem(item['pk_item']).then((Map i) {
+                // Adiciona dados do item a pagina
+                ItemEditPage.item = i;
+
+                // Abre a pagina
+                Navigator.of(context).pushNamed(ItemEditPage.tag);
+              });
+            },
           );
         });
   }
