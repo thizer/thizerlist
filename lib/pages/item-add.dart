4fbeaf32eb2d240c0a3fe1ec496c77ff8fa68328
Marco A. Braghim <marco.a.braghim@gmail.com>
2020-08-22 21:25:38 -0300

    Algumas melhoras nas telas de adicionar e editar items

--- lib/pages/item-add.dart
+++ lib/pages/item-add.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
-
 import 'package:thizerlist/application.dart';
 import 'package:thizerlist/layout.dart';
+import 'package:thizerlist/models/Item.dart';
 import 'package:thizerlist/utils/QuantityFormatter.dart';
+
 import 'items.dart';
 
-import 'package:thizerlist/models/Item.dart';
-
 class ItemAddPage extends StatefulWidget {
-
   static String tag = 'page-item-add';
 
   @override
@@ -17,22 +15,39 @@ class ItemAddPage extends StatefulWidget {
 }
 
 class _ItemAddPageState extends State<ItemAddPage> {
-
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final TextEditingController _cName = TextEditingController();
-  final TextEditingController _cQtd = TextEditingController(text: '1');
-  final MoneyMaskedTextController _cValor =MoneyMaskedTextController(
+  final _formKey = GlobalKey<FormState>();
+  final _cName = TextEditingController();
+  final _cQtd = TextEditingController(text: '1');
+  final _cValor = MoneyMaskedTextController(
     thousandSeparator: '.',
     decimalSeparator: ',',
-    leftSymbol: 'R\$ '
+    leftSymbol: 'R\$ ',
   );
+
+  final _qtdFocus = FocusNode();
 
   String selectedUnit = unity.keys.first;
   bool isSelected = false;
 
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    // Isso vai fazer com que sempre que
+    // o campo de quantidade for selecionado
+    // todo o campo fica selecionado automaticamente
+    // assim o usuario nao precisa apagar o
+    // conteudo para adicionar a nova quantidade
+    _qtdFocus.addListener(() {
+      _cQtd.selection = TextSelection(
+        baseOffset: 0,
+        extentOffset: _cQtd.text.length,
+      );
+    });
 
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final inputName = TextFormField(
       controller: _cName,
       autofocus: true,
@@ -40,8 +55,8 @@ class _ItemAddPageState extends State<ItemAddPage> {
         hintText: 'Nome do item',
         contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
         border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(5)
-        )
+          borderRadius: BorderRadius.circular(5),
+        ),
       ),
       validator: (value) {
         if (value.isEmpty) {
@@ -54,17 +69,17 @@ class _ItemAddPageState extends State<ItemAddPage> {
     final inputQuantidade = TextFormField(
       controller: _cQtd,
       autofocus: false,
+      focusNode: _qtdFocus,
       keyboardType: TextInputType.number,
       decoration: InputDecoration(
         hintText: 'Quantidade',
         contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
         border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(5)
-        )
+          borderRadius: BorderRadius.circular(5),
+        ),
       ),
-      inputFormatters: [ new QuantityFormatter(precision: unity[this.selectedUnit]) ],
+      inputFormatters: [new QuantityFormatter(precision: unity[this.selectedUnit])],
       validator: (value) {
-
         double valueAsDouble = (double.tryParse(value) ?? 0.0);
 
         if (valueAsDouble <= 0) {
@@ -78,7 +93,6 @@ class _ItemAddPageState extends State<ItemAddPage> {
       value: this.selectedUnit,
       onChanged: (String newValue) {
         setState(() {
-
           double valueAsDouble = (double.tryParse(_cQtd.text) ?? 0.0);
           _cQtd.text = valueAsDouble.toStringAsFixed(unity[newValue]);
 
@@ -86,11 +100,8 @@ class _ItemAddPageState extends State<ItemAddPage> {
         });
       },
       items: unity.keys.map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value)
-        );
-      }).toList()
+        return DropdownMenuItem<String>(value: value, child: Text(value));
+      }).toList(),
     );
 
     final inputValor = TextFormField(
@@ -101,8 +112,8 @@ class _ItemAddPageState extends State<ItemAddPage> {
         hintText: 'Valor R\$',
         contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
         border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(5)
-        )
+          borderRadius: BorderRadius.circular(5),
+        ),
       ),
       validator: (value) {
         if (currencyToDouble(value) < 0.0) {
@@ -113,103 +124,95 @@ class _ItemAddPageState extends State<ItemAddPage> {
     );
 
     Container content = Container(
-      child: Form(
-        key: _formKey,
-        child: ListView(
-          shrinkWrap: true,
-          padding: EdgeInsets.all(20),
-          children: <Widget>[
-            Text(
-              'Adicionar Item',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 24
-              ),
-            ),
-            SizedBox(height: 10),
-            Text('Nome do item'),
-            inputName,
-            SizedBox(height: 10),
-            Text('Quantidade'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Container(
-                  width: MediaQuery.of(context).size.width - 150,
-                  child: inputQuantidade,
-                ),
-                Container(width: 100, child: inputUnit)
-              ]
-            ),
-            SizedBox(height: 10),
-            Text('Valor'),
-            inputValor,
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Checkbox(
-                  activeColor: Layout.primary(),
-                  onChanged: (bool value) {
-                    setState(() {
-                      this.isSelected = value;
-                    });
-                  },
-                  value: this.isSelected,
-                ),
-                GestureDetector(
-                  child: Text('Já está no carrinho?', style: TextStyle(fontSize: 18)),
-                  onTap: () {
-                    setState(() {
-                      this.isSelected = !this.isSelected;
-                    });
-                  },
-                )
-              ],
-            ),
-            SizedBox(height: 50),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-              RaisedButton(
-                color: Layout.dark(0.2),
-                child: Text('Cancelar', style:TextStyle(color: Layout.light())),
-                padding: EdgeInsets.only(left: 50, right: 50),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-              RaisedButton(
-                color: Layout.primary(),
-                child: Text('Salvar', style:TextStyle(color: Layout.light())),
-                padding: EdgeInsets.only(left: 50, right: 50),
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-
-                    // Instancia model
-                    ModelItem itemBo = ModelItem();
-
-                    // Adiciona no banco de dados
-                    itemBo.insert({
-                      'fk_lista': ItemsPage.pkList,
-                      'name': _cName.text,
-                      'quantidade': _cQtd.text,
-                      'precisao': unity[this.selectedUnit],
-                      'valor': _cValor.text,
-                      'checked': this.isSelected,
-                      'created': DateTime.now().toString()
-                    }).then((saved) {
-                      Navigator.of(context).pop();
-                      Navigator.of(context).pushReplacementNamed(ItemsPage.tag);
-                    });
-                  }
-                },
-              )
-            ])
-          ]
+        child: Form(
+      key: _formKey,
+      child: ListView(shrinkWrap: true, padding: EdgeInsets.all(20), children: <Widget>[
+        Text(
+          'Adicionar Item',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
         ),
-      )
-    );
+        SizedBox(height: 10),
+        Text('Nome do item'),
+        inputName,
+        SizedBox(height: 10),
+        Text('Quantidade'),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width - 150,
+              child: inputQuantidade,
+            ),
+            Container(width: 100, child: inputUnit)
+          ],
+        ),
+        SizedBox(height: 10),
+        Text('Valor'),
+        inputValor,
+        SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Transform.scale(
+              scale: 2.0,
+              child: Checkbox(
+                activeColor: Layout.primary(),
+                onChanged: (bool value) {
+                  setState(() {
+                    this.isSelected = value;
+                  });
+                },
+                value: this.isSelected,
+              ),
+            ),
+            GestureDetector(
+              child: Text('Já está no carrinho?', style: TextStyle(fontSize: 18)),
+              onTap: () {
+                setState(() {
+                  this.isSelected = !this.isSelected;
+                });
+              },
+            )
+          ],
+        ),
+        SizedBox(height: 10),
+        Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: <Widget>[
+          RaisedButton(
+            color: Layout.dark(0.2),
+            child: Text('Cancelar', style: TextStyle(color: Layout.light())),
+            padding: EdgeInsets.only(left: 50, right: 50),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          RaisedButton(
+            color: Layout.primary(),
+            child: Text('Salvar', style: TextStyle(color: Layout.light())),
+            padding: EdgeInsets.only(left: 50, right: 50),
+            onPressed: () {
+              if (_formKey.currentState.validate()) {
+                // Instancia model
+                ModelItem itemBo = ModelItem();
+
+                // Adiciona no banco de dados
+                itemBo.insert({
+                  'fk_lista': ItemsPage.pkList,
+                  'name': _cName.text,
+                  'quantidade': _cQtd.text,
+                  'precisao': unity[this.selectedUnit],
+                  'valor': _cValor.text,
+                  'checked': this.isSelected,
+                  'created': DateTime.now().toString()
+                }).then((saved) {
+                  Navigator.of(context).pop();
+                  Navigator.of(context).pushReplacementNamed(ItemsPage.tag);
+                });
+              }
+            },
+          )
+        ])
+      ]),
+    ));
 
     return Layout.getContent(context, content, false);
   }
--- lib/pages/item-edit.dart
+++ lib/pages/item-edit.dart
@@ -9,7 +9,6 @@ import 'items.dart';
 import 'package:thizerlist/models/Item.dart';
 
 class ItemEditPage extends StatefulWidget {
-
   static String tag = 'page-item-edit';
   static Map item;
 
@@ -18,22 +17,22 @@ class ItemEditPage extends StatefulWidget {
 }
 
 class _ItemEditPageState extends State<ItemEditPage> {
-
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final TextEditingController _cName = TextEditingController();
-  final TextEditingController _cQtd = TextEditingController();
-  final MoneyMaskedTextController _cValor =MoneyMaskedTextController(
+  final _formKey = GlobalKey<FormState>();
+  final _cName = TextEditingController();
+  final _cQtd = TextEditingController();
+  final _cValor = MoneyMaskedTextController(
     thousandSeparator: '.',
     decimalSeparator: ',',
-    leftSymbol: 'R\$ '
+    leftSymbol: 'R\$ ',
   );
+
+  final _qtdFocus = FocusNode();
 
   String selectedUnit;
   bool isSelected;
 
   @override
   void initState() {
-    
     _cName.text = ItemEditPage.item['name'];
     _cQtd.text = ItemEditPage.item['quantidade'].toString();
     _cValor.text = ItemEditPage.item['valor'];
@@ -45,12 +44,23 @@ class _ItemEditPageState extends State<ItemEditPage> {
       }
     });
 
+    // Isso vai fazer com que sempre que
+    // o campo de quantidade for selecionado
+    // todo o campo fica selecionado automaticamente
+    // assim o usuario nao precisa apagar o
+    // conteudo para adicionar a nova quantidade
+    _qtdFocus.addListener(() {
+      _cQtd.selection = TextSelection(
+        baseOffset: 0,
+        extentOffset: _cQtd.text.length,
+      );
+    });
+
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-
     // Instancia model
     ModelItem itemBo = ModelItem();
 
@@ -61,8 +71,8 @@ class _ItemEditPageState extends State<ItemEditPage> {
         hintText: 'Nome do item',
         contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
         border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(5)
-        )
+          borderRadius: BorderRadius.circular(5),
+        ),
       ),
       validator: (value) {
         if (value.isEmpty) {
@@ -75,17 +85,17 @@ class _ItemEditPageState extends State<ItemEditPage> {
     final inputQuantidade = TextFormField(
       controller: _cQtd,
       autofocus: false,
+      focusNode: _qtdFocus,
       keyboardType: TextInputType.number,
       decoration: InputDecoration(
         hintText: 'Quantidade',
         contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
         border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(5)
-        )
+          borderRadius: BorderRadius.circular(5),
+        ),
       ),
-      inputFormatters: [ new QuantityFormatter(precision: unity[this.selectedUnit]) ],
+      inputFormatters: [new QuantityFormatter(precision: unity[this.selectedUnit])],
       validator: (value) {
-
         double valueAsDouble = (double.tryParse(value) ?? 0.0);
 
         if (valueAsDouble <= 0) {
@@ -99,7 +109,6 @@ class _ItemEditPageState extends State<ItemEditPage> {
       value: this.selectedUnit,
       onChanged: (String newValue) {
         setState(() {
-
           double valueAsDouble = (double.tryParse(_cQtd.text) ?? 0.0);
           _cQtd.text = valueAsDouble.toStringAsFixed(unity[newValue]);
 
@@ -107,11 +116,8 @@ class _ItemEditPageState extends State<ItemEditPage> {
         });
       },
       items: unity.keys.map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value)
-        );
-      }).toList()
+        return DropdownMenuItem<String>(value: value, child: Text(value));
+      }).toList(),
     );
 
     final inputValor = TextFormField(
@@ -122,8 +128,8 @@ class _ItemEditPageState extends State<ItemEditPage> {
         hintText: 'Valor R\$',
         contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
         border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(5)
-        )
+          borderRadius: BorderRadius.circular(5),
+        ),
       ),
       validator: (value) {
         if (currencyToDouble(value) < 0.0) {
@@ -134,103 +140,89 @@ class _ItemEditPageState extends State<ItemEditPage> {
     );
 
     Container content = Container(
-      child: Form(
-        key: _formKey,
-        child: ListView(
-          shrinkWrap: true,
-          padding: EdgeInsets.all(20),
-          children: <Widget>[
-            Text(
-              "Editar: '"+ItemEditPage.item['name'].toString()+"'",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 24
-              ),
-            ),
-            SizedBox(height: 10),
-            Text('Nome do item'),
-            inputName,
-            SizedBox(height: 10),
-            Text('Quantidade'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Container(
-                  width: MediaQuery.of(context).size.width - 150,
-                  child: inputQuantidade,
-                ),
-                Container(width: 100, child: inputUnit)
-              ]
-            ),
-            SizedBox(height: 10),
-            Text('Valor'),
-            inputValor,
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Checkbox(
-                  activeColor: Layout.primary(),
-                  onChanged: (bool value) {
-                    setState(() {
-                      this.isSelected = value;
-                    });
-                  },
-                  value: this.isSelected,
-                ),
-                GestureDetector(
-                  child: Text('Já está no carrinho?', style: TextStyle(fontSize: 18)),
-                  onTap: () {
-                    setState(() {
-                      this.isSelected = !this.isSelected;
-                    });
-                  },
-                )
-              ],
-            ),
-            SizedBox(height: 50),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-              RaisedButton(
-                color: Layout.dark(0.2),
-                child: Text('Cancelar', style:TextStyle(color: Layout.light())),
-                padding: EdgeInsets.only(left: 50, right: 50),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-              RaisedButton(
-                color: Layout.primary(),
-                child: Text('Salvar', style:TextStyle(color: Layout.light())),
-                padding: EdgeInsets.only(left: 50, right: 50),
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-
-                    // Adiciona no banco de dados
-                    itemBo.update(
-                      {
-                        'fk_lista': ItemsPage.pkList,
-                        'name': _cName.text,
-                        'quantidade': _cQtd.text,
-                        'precisao': unity[this.selectedUnit],
-                        'valor': _cValor.text,
-                        'checked': this.isSelected,
-                        'created': DateTime.now().toString()
-                      },
-                      ItemEditPage.item['pk_item']
-                    ).then((saved) {
-                      Navigator.of(context).pop();
-                      Navigator.of(context).pushReplacementNamed(ItemsPage.tag);
-                    });
-                  }
-                },
-              )
-            ])
-          ]
+        child: Form(
+      key: _formKey,
+      child: ListView(shrinkWrap: true, padding: EdgeInsets.all(20), children: <Widget>[
+        Text(
+          "Editar: '" + ItemEditPage.item['name'].toString() + "'",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
         ),
-      )
-    );
+        SizedBox(height: 10),
+        Text('Nome do item'),
+        inputName,
+        SizedBox(height: 10),
+        Text('Quantidade'),
+        Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width - 150,
+            child: inputQuantidade,
+          ),
+          Container(width: 100, child: inputUnit)
+        ]),
+        SizedBox(height: 10),
+        Text('Valor'),
+        inputValor,
+        SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Transform.scale(
+              scale: 2.0,
+              child: Checkbox(
+                activeColor: Layout.primary(),
+                onChanged: (bool value) {
+                  setState(() {
+                    this.isSelected = value;
+                  });
+                },
+                value: this.isSelected,
+              ),
+            ),
+            GestureDetector(
+              child: Text('Já está no carrinho?', style: TextStyle(fontSize: 18)),
+              onTap: () {
+                setState(() {
+                  this.isSelected = !this.isSelected;
+                });
+              },
+            )
+          ],
+        ),
+        SizedBox(height: 10),
+        Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: <Widget>[
+          RaisedButton(
+            color: Layout.dark(0.2),
+            child: Text('Cancelar', style: TextStyle(color: Layout.light())),
+            padding: EdgeInsets.only(left: 50, right: 50),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          RaisedButton(
+            color: Layout.primary(),
+            child: Text('Salvar', style: TextStyle(color: Layout.light())),
+            padding: EdgeInsets.only(left: 50, right: 50),
+            onPressed: () {
+              if (_formKey.currentState.validate()) {
+                // Adiciona no banco de dados
+                itemBo.update({
+                  'fk_lista': ItemsPage.pkList,
+                  'name': _cName.text,
+                  'quantidade': _cQtd.text,
+                  'precisao': unity[this.selectedUnit],
+                  'valor': _cValor.text,
+                  'checked': this.isSelected,
+                  'created': DateTime.now().toString()
+                }, ItemEditPage.item['pk_item']).then((saved) {
+                  Navigator.of(context).pop();
+                  Navigator.of(context).pushReplacementNamed(ItemsPage.tag);
+                });
+              }
+            },
+          )
+        ])
+      ]),
+    ));
 
     return Layout.getContent(context, content, false);
   }
